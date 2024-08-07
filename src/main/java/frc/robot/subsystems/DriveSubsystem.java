b6d109c3fd5c1681ed8ab8ebdb04d2665863511f
FromMarkham <bwang@markhamfirebirds.ca>
2024-08-07 17:38:33 +0200

    Moved speed controller to outside the drive subsystem class

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,16 +4,23 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+//import com.revrobotics.RelativeEncoder;
+//import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
+  private final CANSparkMax m_leftLeaderMotor = new CANSparkMax(DriveConstants.leftBackCAN,CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_leftFollowerMotor = new CANSparkMax(DriveConstants.leftFrontCAN,CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_rightLeaderMotor = new CANSparkMax(DriveConstants.rightFrontCAN,CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_rightFollowerMotor = new CANSparkMax(DriveConstants.rightBackCAN,CANSparkMax.MotorType.kBrushless);
   public DriveSubsystem() {
-    private final CANSparkMax m_leftLeaderMotor = new CANSparkMax(DriveConstants.leftBackCAN,CANSparkMax.MotorType.kBrushless);
-    private final CANSparkMax m_leftFollowerMotor = new CANSparkMax(DriveConstants.leftFrontCAN,CANSparkMax.MotorType.kBrushless);
-    private final CANSparkMax m_rightLeaderMotor = new CANSparkMax(DriveConstants.rightFrontCAN,CANSparkMax.MotorType.kBrushless);
-    private final CANSparkMax m_rightFollowerMotor = new CANSparkMax(DriveConstants.rightBackCAN,CANSparkMax.MotorType.kBrushless);
+
   }
 
   @Override
