e678625bdda046c8d60566773b48e0ed827fc5bc
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-08-08 11:25:17 -0400

    added more commands for the drivesubsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,12 +5,14 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 //import com.revrobotics.RelativeEncoder;
 //import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.Constants.DriveConstants;
 
@@ -29,13 +31,37 @@ public class DriveSubsystem extends SubsystemBase {
   private final CANSparkMax m_rightLeaderMotor = new CANSparkMax(DriveConstants.rightFrontCAN,CANSparkMax.MotorType.kBrushless);
   private final CANSparkMax m_rightFollowerMotor = new CANSparkMax(DriveConstants.rightBackCAN,CANSparkMax.MotorType.kBrushless);
   
-  // 
+  // Allows interfacing with the integrated PID Controller on the motors.
   private final SparkPIDController leftSpeedPID = m_leftLeaderMotor.getPIDController();
   private final SparkPIDController rightSpeedPID = m_rightLeaderMotor.getPIDController();
   
+  // Instantiates the DifferentialDrive class
+  private final DifferentialDrive m_drive;
+
+  // ENCODER DECLARATION
+  private final RelativeEncoder m_leftEncoder = m_leftLeaderMotor.getEncoder();
+  private final RelativeEncoder m_rightEncoder = m_rightLeaderMotor.getEncoder();
+
 
   public DriveSubsystem() {
     // 
+
+    // Ensures motors are loaded with the current config, not with a previous config.
+    m_leftLeaderMotor.restoreFactoryDefaults();
+    m_leftFollowerMotor.restoreFactoryDefaults();
+    m_rightLeaderMotor.restoreFactoryDefaults();
+    m_rightFollowerMotor.restoreFactoryDefaults();    
+
+    // Inverts the leader motor of each side so that the motors aren't going against each other
+    m_leftLeaderMotor.setInverted(true);
+    m_rightLeaderMotor.setInverted(true);
+
+    // Any updates made to the Leader Motor will additionally be made to the Follower Motor, even without directly updating the Follower Motor
+    m_leftFollowerMotor.follow(m_leftLeaderMotor);
+    m_rightFollowerMotor.follow(m_rightLeaderMotor);
+
+    // 
+    m_drive = new DifferentialDrive(m_leftLeaderMotor, m_rightLeaderMotor);
   }
 
   @Override
