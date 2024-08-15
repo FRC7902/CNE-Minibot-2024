b785b0b8e77ea22a25a587c0c0e5a34d9cd66243
Hayley So <hayleyso99@gmail.com>
2024-08-15 13:47:35 -0400

    added basic commands and functions

---
+++ src/main/java/ArmUtils.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import frc.robot.Constants.ArmConstants;
+
+/** Add your docs here. */
+public class ArmUtils {
+
+    public int radsToCTRESensorUnits(double angleInRads, int encoderCPR) {
+        return (int) (angleInRads * encoderCPR / 2 * Math.PI / ArmConstants.GearRatio);
+    }
+
+    public double degToCTRESensorUnits(double angleInDeg, int encoderCPR) {
+        return (angleInDeg * ArmConstants.GearRatio * encoderCPR / 360);
+    }
+
+    public double CTRESensorUnitsToDeg(double angleInSensorUnits, int encoderCPR) {
+        return (angleInSensorUnits * 360.0 / encoderCPR * ArmConstants.GearRatio);
+    }
+
+    public double CTRESensorUnitsToRads(double angleInSensorUnits, int encoderCPR) {
+        return (angleInSensorUnits * 2 * Math.PI * ArmConstants.GearRatio / encoderCPR);
+    }
+
+}
---
+++ src/main/java/frc/robot/commands/arm/MoveArmUpCmd.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class MoveArmUpCmd extends Command {
+  private ArmSubsystem m_armSubsystem;
+  private static final double ARM_SPEED = -0.5;
+
+  /** Creates a new MoveArmDownCmd. */
+  public MoveArmUpCmd(ArmSubsystem arm) {
+    m_armSubsystem = arm;
+    addRequirements(arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Move arm up if not at upper limit
+    if (m_armSubsystem.getAngle() < ArmConstants.MAX_ANGLE) {
+      m_armSubsystem.setPower(ARM_SPEED);
+    } else {
+      m_armSubsystem.stopMotor();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_armSubsystem.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
