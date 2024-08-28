3cc5e88689bb0d2be3e829ab2228bcd87ef906ee
FromMarkham <bwang@markhamfirebirds.ca>
2024-08-28 12:01:25 -0400

    Added stuff to simulatedrivepid.java

--- src/main/java/frc/robot/commands/SimulateDrivePid.java
+++ src/main/java/frc/robot/commands/SimulateDrivePid.java
@@ -1,5 +1,15 @@
 package frc.robot.commands;
+import frc.robot.subsystems.DriveSubsystem;
+
 
 public class SimulateDrivePid {
-    
+    public void DriveSimulate{
+        var rightleadermotorsim=m_rightLeaderMotor.getSimState();
+        m_rightFollowerMotor;
+        m_leftLeaderMotor;
+        m_leftFollowerMotor;
+    }
+
+
 }
+//https://v6.docs.ctr-electronics.com/en/stable/docs/api-reference/simulation/simulation-intro.html
